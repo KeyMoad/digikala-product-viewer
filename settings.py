f5c4c214001fab97656ecd342f263760c53bbf53
KeyMoad <mohamadreza.najarbashii@gmail.com>
2024-08-25 15:40:04 +0330

    add id list file path variable

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+from os import path
+
+
 # Define the product URL
 BASE_PRODUCT_URL = 'https://www.digikala.com/product'
 DEFAULT_TIMEOUT = 10
@@ -11,3 +14,6 @@ PROXY_LIST_URLS = [
     'https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/proxy.txt',
     'https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt'
 ]
+
+# ID List file path
+ID_LIST_FILE = path.join(path.dirname(path.realpath(__file__)), 'id_list.txt')
