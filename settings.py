9cb7839ac1020e3f29811910a65aff2d40775c81
KeyMoad <mohamadreza.najarbashii@gmail.com>
2024-08-25 14:40:10 +0330

    added settings

---
+++ settings.py
@@ -0,0 +1,13 @@
+# Define the product URL
+BASE_PRODUCT_URL = 'https://www.digikala.com/product'
+DEFAULT_TIMEOUT = 10
+
+# Proxy list
+PROXY_LIST_URLS = [
+    'https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt',
+    'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt',
+    'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt',
+    'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt',
+    'https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/proxy.txt',
+    'https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt'
+]
