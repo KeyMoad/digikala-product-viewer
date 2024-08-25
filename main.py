8aa4f13b2da5248598a47dee2ec83d5182816b0d
KeyMoad <mohamadreza.najarbashii@gmail.com>
2024-08-25 15:40:57 +0330

    add argument to reading view count, update base on changes

--- main.py
+++ main.py
@@ -1,47 +1,59 @@
+from argparse import ArgumentParser
 from selenium.webdriver.support.ui import WebDriverWait
+from sys import exit
 import chromedriver_autoinstaller
 
-from settings import BASE_PRODUCT_URL, DEFAULT_TIMEOUT, PROXY_LIST_URLS
-from src.utils import logger
-from src.viewer import viewer
-from src.driver import get_driver
+from settings import BASE_PRODUCT_URL, DEFAULT_TIMEOUT, PROXY_LIST_URLS, ID_LIST_FILE
+from src.utils import logger, read_product_ids
+from src.viewer import ProductViewer
+from src.driver import DriverManager
 from src.proxy import ProxyManager
 
 
-def main(view_number: int, list_of_id: list):
+def main(view_number: int, product_ids: list):
     """
     Main function to initiate viewing process for each product.
 
     Args:
         view_number (int): Number of views to simulate.
-        list_of_id (list): List of product IDs.
+        product_ids (list): List of product IDs.
     """
-    for product_id in list_of_id:
-        url = PRODUCT_URL(product_id.strip())
-        logger.info(f'Checking product {product_id.strip()} [{url}] ...')
-        viewer(driver, wait, url, view_number)
+    proxy_manager = ProxyManager(PROXY_LIST_URLS)
+    driver_manager = DriverManager(proxy_manager)
+
+    for product_id in product_ids:
+        url = f'{BASE_PRODUCT_URL}/{product_id.strip()}'
+        logger.info(f'Starting view process for product {product_id.strip()} [{url}] ...')
+
+        driver = driver_manager.get_driver()
+        wait = WebDriverWait(driver, DEFAULT_TIMEOUT)
+        viewer = ProductViewer(driver, wait)
+        
+        viewer.simulate_views(url, view_number)
+        
+        driver.quit()
 
 
 if __name__ == '__main__':
-    # Get product ID/s and number of views from user input
-    product_ids = input('Enter product IDs separated by commas: ').strip().split(',')
-    view_number = int(input('Enter the number of views: '))
+    # Setup argument parser
+    parser = ArgumentParser(description='Simulate product views on a digikala.')
+    parser.add_argument('view_number', type=int, help='Number of views to simulate for each product.')
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Read product IDs from the ID_LIST_FILE
+    product_ids = read_product_ids(ID_LIST_FILE)
+    
+    # Check if product_ids is not empty
+    if not product_ids:
+        logger.error(f'No product IDs found in {ID_LIST_FILE} the list. Exiting...')
+        exit(1)
 
     # Automatically install the correct version of chromedriver
     chromedriver_autoinstaller.install()
 
-    proxy_manager = ProxyManager(PROXY_LIST_URLS)
-
-    driver = get_driver(proxy=proxy_manager.get_random_proxy())
-
-    # Wait for elements to be available (timeout after 10 seconds)
-    wait = WebDriverWait(driver, DEFAULT_TIMEOUT)
-
-    # Final url lambda
-    PRODUCT_URL = lambda product_id : f'{BASE_PRODUCT_URL}/{product_id}'
-
-    # Run the main function
     try:
-        main(view_number, product_ids)
-    finally:
-        driver.quit()
+        main(args.view_number, product_ids)
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
