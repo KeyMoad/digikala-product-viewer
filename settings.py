f5386cc45aa5a1a4ef2d50df0a3080cf1a7f78d0
KeyMoad <mohamadreza.najarbashii@gmail.com>
2024-08-26 22:10:40 +0330

    fix proxy list url for socks4 and socks5

--- settings.py
+++ settings.py
@@ -13,11 +13,11 @@ HTTP_PROXY_LIST_URLS = [
 ]
 SOCKS4_PROXY_LIST_URLS = [
     'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt',
-    'https://github.com/sunny9577/proxy-scraper/blob/master/generated/socks4_proxies.txt'
+    'https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/generated/socks4_proxies.txt'
 ]
 SOCKS5_PROXY_LIST_URLS = [
     'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt',
-    'https://github.com/sunny9577/proxy-scraper/blob/master/generated/socks5_proxies.txt'
+    'https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/generated/socks5_proxies.txt'
 ]
 
 # ID List file path
