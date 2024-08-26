b406a39779d69df83f539726592ddf347638932d
KeyMoad <mohamadreza.najarbashii@gmail.com>
2024-08-26 19:12:16 +0330

    make driver headless

--- src/driver.py
+++ src/driver.py
@@ -19,7 +19,7 @@ class DriverManager:
 
         us = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
         chrome_options = Options()
-        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
