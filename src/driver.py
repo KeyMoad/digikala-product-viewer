8b600b5f81f6c5ee9f2d4e9120ebde3121b75f14
KeyMoad <mohamadreza.najarbashii@gmail.com>
2024-08-26 19:16:24 +0330

    add incognito argument to diver

--- src/driver.py
+++ src/driver.py
@@ -23,6 +23,7 @@ class DriverManager:
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument('--incognito')
         chrome_options.add_argument(f"user-agent={us}")
 
         # Add the proxy to Chrome options
