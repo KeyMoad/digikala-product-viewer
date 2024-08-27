defafc949f9d56846b18a817dbe9bd2a63bd86ef
KeyMoad <mohamadreza.najarbashii@gmail.com>
2024-08-27 22:02:58 +0330

    add proxy file argument fot custom proxy list

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 import chromedriver_autoinstaller
 
 from settings import BASE_PRODUCT_URL, DEFAULT_TIMEOUT, ID_LIST_FILE, HTTP_PROXY_LIST_URLS, SOCKS4_PROXY_LIST_URLS, SOCKS5_PROXY_LIST_URLS
-from src.utils import logger, read_product_ids
+from src.utils import logger, read_file
 from src.viewer import ProductViewer
 from src.driver import DriverManager
 from src.proxy import ProxyManager
@@ -63,7 +63,7 @@ def view_product_in_batches(product_id: str, view_number: int, batch_size: int, 
     logger.info(f'Completed viewing process for product {product_id.strip()}')
 
 
-def main(view_number: int, product_ids: list, batch_size: int, proxy_type: str, proxy_test_type: str):
+def main(view_number: int, product_ids: list, batch_size: int, proxy_type: str, proxy_test_type: str, proxy_file: str):
     """
     Main function to initiate viewing process for each product in batches.
 
@@ -72,14 +72,12 @@ def main(view_number: int, product_ids: list, batch_size: int, proxy_type: str, 
         product_ids (list): List of product IDs.
         batch_size (int): Number of concurrent views to run in each batch.
     """
-    need_to_test = True if proxy_test_type else False
-
     if proxy_type == 'http':
-        proxy_manager = ProxyManager(HTTP_PROXY_LIST_URLS, proxy_type, BASE_PRODUCT_URL, proxy_test_type, need_to_test)
+        proxy_manager = ProxyManager(HTTP_PROXY_LIST_URLS, proxy_type, BASE_PRODUCT_URL, proxy_test_type, proxy_file)
     elif proxy_type == 'socks4':
-        proxy_manager = ProxyManager(SOCKS4_PROXY_LIST_URLS, proxy_type, BASE_PRODUCT_URL, proxy_test_type, need_to_test)
+        proxy_manager = ProxyManager(SOCKS4_PROXY_LIST_URLS, proxy_type, BASE_PRODUCT_URL, proxy_test_type, proxy_file)
     elif proxy_type == 'socks5':
-        proxy_manager = ProxyManager(SOCKS5_PROXY_LIST_URLS, proxy_type, BASE_PRODUCT_URL, proxy_test_type, need_to_test)
+        proxy_manager = ProxyManager(SOCKS5_PROXY_LIST_URLS, proxy_type, BASE_PRODUCT_URL, proxy_test_type, proxy_file)
     else:
         raise ValueError(f"Unsupported proxy type: {proxy_type}")
 
@@ -96,15 +94,16 @@ if __name__ == '__main__':
     parser.add_argument('--batch-size', type=int, default=10, help='Number of concurrent views to run in each batch. Default [10]')
     parser.add_argument('--proxy-type', type=str, default='http', choices=['http', 'socks4', 'socks5'], help='The proxy type of connections. Default [http]')
     parser.add_argument('--proxy-test-type', type=str, default='', choices=['driver', 'request'], help='The type of proxy validation test. The driver mode takes more time but provides more accurate results, while the request mode is faster but offers less thorough validation. [If not passed, no test will be done]')
+    parser.add_argument('--proxy-file', type=str, default='', help='If you want to use your own proxy list, pass the file path. File format should be txt and put your proxies addresses with this syntax: IP:PORT. [proxy-type is necessary]')
     args = parser.parse_args()
 
     # Automatically install the correct version of chromedriver
     chromedriver_autoinstaller.install()
 
     # Read product IDs from file
-    product_ids = read_product_ids(ID_LIST_FILE)
+    product_ids = read_file(ID_LIST_FILE)
 
     try:
-        main(args.view_number, product_ids, args.batch_size, args.proxy_type, args.proxy_test_type)
+        main(args.view_number, product_ids, args.batch_size, args.proxy_type, args.proxy_test_type, args.proxy_file)
     except Exception as e:
         logger.error(f"An error occurred: {e}")
