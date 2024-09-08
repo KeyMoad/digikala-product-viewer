b1a765ee31ae3453bbee8db9cd8665291ea52ea6
KeyMoad <mohamadreza.najarbashii@gmail.com>
2024-09-08 19:08:31 +0330

    added config based run feature

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from random import uniform
 import chromedriver_autoinstaller
 
 from settings import BASE_PRODUCT_URL, DEFAULT_TIMEOUT, ID_LIST_FILE, HTTP_PROXY_LIST_URLS, SOCKS4_PROXY_LIST_URLS, SOCKS5_PROXY_LIST_URLS
+from src.config import load_config, merge_args_with_config
 from src.utils import logger, read_file
 from src.viewer import ProductViewer
 from src.driver import DriverManager
@@ -107,28 +108,35 @@ def main(view_number: int, product_ids: list, batch_size: int, proxy_type: str, 
 
 
 if __name__ == '__main__':
-    import argparse
+    from argparse import ArgumentParser
 
     # Argument parser for command line arguments
-    parser = argparse.ArgumentParser(description='Simulate product views.')
-    parser.add_argument('--view-number', type=int, default=50, help='Number of views to simulate per product. Default [50]')
-    parser.add_argument('--batch-size', type=int, default=10, help='Number of concurrent views to run in each batch. Default [10]')
-    parser.add_argument('--proxy-type', type=str, default='http', choices=['http', 'socks4', 'socks5'], help='The proxy type of connections. Default [http]')
-    parser.add_argument('--proxy-test-type', type=str, default='', choices=['driver', 'request'], help='The type of proxy validation test. The driver mode takes more time but provides more accurate results, while the request mode is faster but offers less thorough validation. [If not passed, no test will be done]')
-    parser.add_argument('--proxy-file', type=str, default='', help='If you want to use your own proxy list, pass the file path. File format should be txt and put your proxies addresses with this syntax: IP:PORT. [proxy-type is necessary]')
+    parser = ArgumentParser(description='Simulate product views.')
+    parser.add_argument('--config', type=str, help='Path to the config file.')
+    parser.add_argument('--view-number', type=int, help='Number of views to simulate per product. Default [50]')
+    parser.add_argument('--batch-size', type=int, help='Number of concurrent views to run in each batch. Default [10]')
+    parser.add_argument('--proxy-type', type=str, choices=['http', 'socks4', 'socks5'], help='The proxy type of connections. Default [http]')
+    parser.add_argument('--proxy-test-type', type=str, choices=['driver', 'request'], help='The type of proxy validation test. The driver mode takes more time but provides more accurate results, while the request mode is faster but offers less thorough validation. [If not passed, no test will be done]')
+    parser.add_argument('--proxy-file', type=str, help='If you want to use your own proxy list, pass the file path. File format should be txt and put your proxies addresses with this syntax: IP:PORT. [proxy-type is necessary]')
     parser.add_argument('--premium-proxy', action='store_true', help='If passed, indicates that the proxy list contains premium proxies.')
-    parser.add_argument('--username', type=str, default=None, help='Username for premium proxy authentication.')
-    parser.add_argument('--password', type=str, default=None, help='Password for premium proxy authentication.')
+    parser.add_argument('--username', type=str, help='Username for premium proxy authentication.')
+    parser.add_argument('--password', type=str, help='Password for premium proxy authentication.')
 
     args = parser.parse_args()
+
+    # Load config file if provided
+    config = load_config(args.config)
+    # Merge arguments with config values
+    merged_args = merge_args_with_config(args, config)
 
     # Automatically install the correct version of chromedriver
     chromedriver_autoinstaller.install()
 
-    if args.premium_proxy and (not args.proxy_file):
+    # Validate necessary arguments
+    if merged_args['premium_proxy'] and (not merged_args['proxy_file']):
         logger.error('Please provide premium proxy list as a file.')
         exit(1)
-    if args.premium_proxy and (not args.username or not args.password):
+    if merged_args['premium_proxy'] and (not merged_args['username'] or not merged_args['password']):
         logger.error('Premium proxy requires both a username and a password.')
         exit(1)
 
@@ -139,6 +147,16 @@ if __name__ == '__main__':
         exit(1)
 
     try:
-        main(args.view_number, product_ids, args.batch_size, args.proxy_type, args.proxy_test_type, args.proxy_file, args.premium_proxy, args.username, args.password)
+        main(
+            merged_args['view_number'],
+            product_ids,
+            merged_args['batch_size'],
+            merged_args['proxy_type'],
+            merged_args['proxy_test_type'],
+            merged_args['proxy_file'],
+            merged_args['premium_proxy'],
+            merged_args['username'],
+            merged_args['password']
+        )
     except Exception as e:
         logger.error(f"An error occurred: {e}")
---
+++ src/config.py
@@ -0,0 +1,24 @@
+from yaml import safe_load
+
+def load_config(config_file_path: str):
+    """Loads the configuration from the YAML file."""
+    with open(config_file_path, 'r') as file:
+        config = safe_load(file)
+    return config
+
+
+def merge_args_with_config(args, config) -> dict:
+    """Merge command line arguments with config values, giving priority to args."""
+    def get_value(arg_value, config_value, default_value):
+        return arg_value if arg_value is not None else config_value if config_value is not None else default_value
+
+    return {
+        'view_number': get_value(args.view_number, config.get('view_number'), 50),
+        'batch_size': get_value(args.batch_size, config.get('batch_size'), 5),
+        'proxy_type': get_value(args.proxy_type, config.get('proxy_type'), 'http'),
+        'proxy_test_type': get_value(args.proxy_test_type, config.get('proxy_test_type'), ''),
+        'proxy_file': get_value(args.proxy_file, config.get('proxy_file'), ''),
+        'premium_proxy': args.premium_proxy if args.premium_proxy is not None else config.get('premium_proxy', False),
+        'username': get_value(args.username, config.get('username'), None),
+        'password': get_value(args.password, config.get('password'), None)
+    }
